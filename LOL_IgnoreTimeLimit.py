7c0a7c32d0d0ca9aa49cd14a64a689795ae63c81
SaoHYC <67623049+SaoHYC@users.noreply.github.com>
2021-11-13 08:17:42 -0600

    Added skip WeGame patch.
    A major refactoring with a patch that skips WeGame and removes some unnecessary features.

--- LOL_IgnoreTimeLimit.py
+++ LOL_IgnoreTimeLimit.py
@@ -1,34 +1,36 @@
-import os,easygui,sys,win32api,requests
+import os,easygui,sys,win32api,stat
 
-#定义联网获取源json的还原函数
-#Define the restore function to get the source JSON through networking.
-def back(file):
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
-    try:
-        r = requests.get('https://raw.githubusercontent.com/SaoHYC/LOL_IgnoreTimeLimit/Main/plugin-manifest.json' , headers = headers)
-    except:
-        easygui.msgbox(msg = '无法与互联网建立连接，请再次重试' , title = '网络连接失败' , ok_button = '好的')
-        sys.exit()
-    r = r.text
-    with open('test.txt' , 'w+') as file_write:
-        file_write.write(r)
-    with open('test.txt' , 'r+') as file_write:
-        lines = file_write.readlines()
-    del lines[-2:]
-    file_rstrip = open('test2.txt' , 'a+')
-    for line in lines:
-        line = line.rstrip()
-        if len(line) != 0:
-            file_rstrip.writelines(line)
-            file_rstrip.write('\n')
-    file_rstrip.close()
-    with open('test2.txt' , 'a+') as file_add:
-        file_add.write('}')
-    with open('test2.txt' , 'r+') as lastestfile:
-        the_backup = lastestfile.read()
-    file.write(the_backup)
-    os.remove('test.txt')
-    os.remove('test2.txt')
+#安装跳过WeGame补丁
+#Install Skip_WeGame patch
+def WeGameFix():
+    os.chmod(r'.\TCLS\wegame_launch.ini',stat.S_IWRITE)
+    wegame_launch_ini = open(r'.\TCLS\wegame_launch.ini' , 'w+')
+    wegame_launch_ini.write('[TCLS]\ndata_name=lol')
+    wegame_launch_ini.close()
+    os.chmod(r'.\TCLS\wegame_launch.ini',stat.S_IREAD)
+    os.chmod(r'.\TCLS\wegame_launch.tmp',stat.S_IWRITE)
+    wegame_launch_tmp = open(r'.\TCLS\wegame_launch.tmp' , 'w+')
+    wegame_launch_tmp.write('[TCLS]\nLastLoginMethod=1\nLocalR=99')
+    wegame_launch_tmp.close()
+    os.chmod(r'.\TCLS\wegame_launch.tmp',stat.S_IREAD)
+    easygui.msgbox(msg = '已成功安装！', title = '完成' , ok_button = '好的')
+
+#是否需要跳过WeGame
+#Whether to skip WeGame
+def WeGame_need_fix():
+    suggest = 0
+    with open(r'.\TCLS\wegame_launch.ini','r') as WeGame_launch:
+        WeGame_launch = WeGame_launch.read()
+    if '''[TCLS]
+data_name=lol''' not in WeGame_launch:
+        suggest = 1
+    with open(r'.\TCLS\wegame_launch.tmp','r') as WeGame_launch:
+        WeGame_launch = WeGame_launch.read()
+    if '''[TCLS]
+LastLoginMethod=1
+LocalR=99''' not in WeGame_launch:
+        suggest = 1
+    return suggest
 
 #定义一个统计函数
 #Define a statistical function.
@@ -88,7 +90,7 @@ def replace_json():
             t += 1
         else:
             read4 = read3
-
+        os.chdir('.\LeagueClient\Plugins')
         with open('plugin-manifest2.json' , 'w+') as replace:
             replace.write(read4)
         os.remove('plugin-manifest.json')
@@ -96,7 +98,7 @@ def replace_json():
         easygui.msgbox(msg = '已成功替换{}处'.format(t) , title = '完成' , ok_button = '好的')
 
 #定义一个获取exe文件版本的函数
-#Define a function to get the version of the EXE file.
+#Define a function to get the version of the exe file.
 def getFileVersion(file_name):
     info = win32api.GetFileVersionInfo(file_name, os.sep)
     ms = info['FileVersionMS']
@@ -104,9 +106,23 @@ def getFileVersion(file_name):
     version = '%d.%d.%d.%04d' % (win32api.HIWORD(ms), win32api.LOWORD(ms), win32api.HIWORD(ls), win32api.LOWORD(ls))
     return version
 
+times = 0
+
+#判断是否有路径配置文件
+#Judge whether there is a path configuration file.
+Settingpath = 'C:' + os.environ['HOMEPATH'] + r'\AppData\Roaming\LOL_TimeLimit\Setting.txt'
+if os.path.exists(Settingpath):
+    Spath = open(Settingpath , 'r+')
+    Spathline = Spath.readlines()
+    Spath.close()
+    Spath = Spathline[1]
+    Spath = Spath.lstrip('[PATH]=')
+    os.chdir(Spath)
+elif not os.path.exists('C:' + os.environ['HOMEPATH'] + r'\AppData\Roaming\LOL_TimeLimit'):
+    os.mkdir('C:' + os.environ['HOMEPATH'] + r'\AppData\Roaming\LOL_TimeLimit')
+
 #循环检测路径直到匹配
 #Loop the path until the match is successful.
-times = 0
 while not os.path.isdir('LeagueClient') and times == 0:
     times += 1
     choice = easygui.buttonbox(msg = '没有检测到当前目录下的英雄联盟相关文件，请手动选择英雄联盟游戏目录！' , title = '手动选择' , choices = ['选择' , '取消'])
@@ -123,35 +139,46 @@ while not os.path.isdir('LeagueClient') and times == 1:
     else:
         sys.exit()
 
+#写入路径配置文件
+#Write the path configuration file.
+with open(Settingpath,'w+') as Settingpath:
+    Settingpath.write('#该文件为路径配置文件请勿删除！' + '\n[PATH]=' + Spath)
+
+#获取游戏版本
+#Get the game version.
 Game_Version = getFileVersion(r'.\Game\League of Legends.exe')
 
-#更改工作路径并读进read
-#Change working path and read in 'read'.
-os.chdir(r'.\LeagueClient\Plugins')
-with open('plugin-manifest.json' , 'r+') as file:
+#读进read
+#Read in 'read'.
+with open(r'.\LeagueClient\Plugins\plugin-manifest.json' , 'r+') as file:
         read = file.read()
 
-#当不需要修改时执行的操作
-#Operation when no modification is required.
-if Statistics() == 0:
-    choice = easygui.buttonbox(msg = '不需要再修改了，但可以还原文件',title = '提示',choices = ['还原为官方文件' , '退出'])
-    if choice == '还原为官方文件':
-        with open('plugin-manifest.json' , 'w+') as backup:
-            back(backup)
-        easygui.msgbox(msg = '已成功还原' , title = '还原' , ok_button = '好的')
-    easygui.msgbox(msg = '有疑问或bug反馈请在Github上提交issues\n\n源码参见:https://github.com/SaoHYC/LOL_IgnoreTimeLimit',title = 'Debug',ok_button = '好的')
+#先锁文件防傻狗TX乱改
+#Lock the file to prevent TX from messing up.
+os.chmod(r'.\TCLS\wegame_launch.tmp',stat.S_IREAD)
+os.chmod(r'.\TCLS\wegame_launch.ini',stat.S_IREAD)
+#执行的操作
+#Operation.
+if Statistics() == 0 and WeGame_need_fix() == 0:
+    easygui.msgbox(msg = '您无需修改',title = '提示',ok_button = '好的')
+    easygui.msgbox(msg = '有疑问或bug反馈请在Github上提交issues\n\n源码参见:https://github.com/SaoHYC/LOL_IgnoreTimeLimit' , title = 'Debug' , ok_button = '好的')
     sys.exit()
-choice = easygui.buttonbox(msg = '目前可以修改{num}处地方(共3处)\n\n选择操作：'.format(num = Statistics()) ,title = '适配游戏版本{}'.format(Game_Version),choices = ['修改文件','还原为官方文件','取消'])
+elif Statistics() != 0 and WeGame_need_fix() == 0:
+    choice = easygui.buttonbox(msg = '目前可以修改{num}处地方解除限时(共3处)\n\n选择操作：'.format(num = Statistics()) ,title = '适配游戏版本{}'.format(Game_Version),choices = ['修改文件','取消'])
+elif Statistics() == 0 and WeGame_need_fix() != 0:
+    choice = easygui.buttonbox(msg = '目前可以破解WeGame强制登录，是否安装补丁？',title = '适配游戏版本{}'.format(Game_Version),choices = ['安装补丁','取消'])
+elif Statistics() != 0 and WeGame_need_fix() != 0:
+    choice = easygui.buttonbox(msg = '目前可以修改{num}处地方解除限时(共3处)\n\n可以安装跳过WeGame补丁\n\n选择操作：'.format(num = Statistics()) ,title = '适配游戏版本  {}'.format(Game_Version),choices = ['修改文件','安装补丁','一键修复','取消'])
 
-#调用replace_json函数
-#Call replace_json function.
+#调用函数
+#Call function.
 if choice == '修改文件':
     replace_json()
-
-#恢复源文件
-#Restore files.
-elif choice == '还原为官方文件':
-    with open('plugin-manifest.json' , 'w+') as backup:
-        back(backup)
-    easygui.msgbox(msg = '已成功还原' , title = '还原' , ok_button = '好的')
+if choice == '安装补丁':
+    WeGameFix()
+if choice == '一键修复':
+    WeGameFix()
+    replace_json()
+if choice == '取消':
+    easygui.msgbox(msg = '操作已取消' , title = 'Cancel' , ok_button = '好的')
 easygui.msgbox(msg = '有疑问或bug反馈请在Github上提交issues\n\n源码参见:https://github.com/SaoHYC/LOL_IgnoreTimeLimit' , title = 'Debug' , ok_button = '好的')
